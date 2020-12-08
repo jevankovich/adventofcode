a621759cbe465718be45dbd0f92dcb678d95ef37
Justin Evankovich <justinevankovich@gmail.com>
2020-12-08 17:38:57 -0600

    day 7

---
+++ 2020/day7/Main.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import qualified Data.Tree as T
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+
+import Data.List (foldl')
+
+import Text.Parsec
+import Text.Parsec.Char
+
+type Parser = Parsec String ()
+
+seed = "shiny gold"
+
+main :: IO ()
+main = do
+    input <- getContents
+    relation <- case parse (many clause) "stdin" input of
+        Left e -> do 
+            print e
+            return M.empty
+        Right cs -> do
+            return $ M.fromList cs
+    let inv = inverse . M.map (map snd) $ relation
+    let invTrans = transitive inv
+    print $ subtract 1 . S.size  <$> S.fromList <$> T.flatten <$> M.lookup seed invTrans
+    print $ countBags relation seed
+    return ()
+
+inverse :: (Ord a, Ord b) => M.Map a [b] -> M.Map b [a]
+inverse = foldl' inverse' M.empty . M.toList where
+    inverse' m (k, vs) = foldl' (\m v -> M.insertWith (++) v [k] m) m vs
+
+transitive :: (Ord a) => M.Map a [a] -> M.Map a (T.Tree a)
+transitive m = M.mapWithKey (\k _ -> transitive' k m) m where
+    transitive' k m = T.unfoldTree (\k -> (k, concat $ M.lookup k m)) k
+
+countBags :: M.Map String [(Int, String)] -> String -> Int
+countBags m b = countBags' b - 1 where -- -1 since we don't count the root bag
+    countBags' :: String -> Int
+    countBags' b = case M.lookup b m of
+        Nothing -> error $ "Non-existent bag type " ++ b
+        Just bs -> 1 + (sum . map (\(n, b) -> n * countBags' b) $ bs)
+
+bag :: Parser String
+bag = do
+    modifier <- many1 letter
+    space
+    adjective <- many1 letter
+    space
+    string "bag"
+    optional $ char 's'
+    return $ modifier ++ " " ++ adjective
+
+nbags :: Parser (Int, String)
+nbags = do
+    n <- many1 digit
+    space
+    b <- bag
+    return (read n, b)
+    
+nbagsList = (string "no other bags" >> return []) <|> (do
+    first <- nbags
+    rest <- many (string ", " >> nbags)
+    return $ first:rest)
+
+clause :: Parser (String, [(Int, String)])
+clause = do
+    from <- bag
+    string " contain "
+    bagsList <- nbagsList
+    string ".\n"
+    return (from, bagsList)
